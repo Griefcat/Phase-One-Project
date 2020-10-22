dd5d88cb30e6a06183927663f2318a2c7eb62639
Griefcat <anardolilli@gmail.com>
2020-10-22 11:01:06 -0400

    looks better with spaces

--- apps/models/welcome.rb
+++ apps/models/welcome.rb
@@ -1,24 +1,20 @@
 require 'pastel'
 def welcome
     pastel = Pastel.new
-    puts " "
-    puts pastel.green("  Buongiorno  ") + (" and ") + pastel.red("  Congratulazioni!  ")
-    puts " "
-    puts "You've decided to take the best trip on planet earth --"
+    puts pastel.green("\n  Buongiorno  ") + (" and ") + pastel.red("  Congratulazioni!  ")
+    puts "\n You've decided to take the best trip on planet earth --"
     sleep(2)
-    puts " "
-    puts "to the breathtaking"
-    puts " "
+    puts "\n to the breathtaking"
     sleep(1)
-    puts pastel.blue("
+    puts pastel.blue(" \n
     ▄▀█ █▀▄▀█ ▄▀█ █░░ █▀▀ █    █▀▀ █▀█ ▄▀█ █▀ ▀█▀
     █▀█ █░▀░█ █▀█ █▄▄ █▀░ █    █▄▄ █▄█ █▀█ ▄█ ░█░")
     sleep(5)
-    puts " "
+    
 end
 
 def budget
- puts "One a scale of 1-5 (1 being a cheapskate and 5 being Jeff Bezos), what is your budget?"
+ puts "\n One a scale of 1-5 (1 being a cheapskate and 5 being Jeff Bezos), what is your budget?"
  vistor_budget = gets.chomp
  return vistor_budget
 end
